25353e6942b52c766880a5bc9bc7eccb6aa87957
AlbanBD <albanbezieau@gmail.com>
2019-10-30 10:21:29 +0100

    Ajout de commentaire

--- meetingRoom/src/app/app.component.ts
+++ meetingRoom/src/app/app.component.ts
@@ -9,6 +9,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   date:Date;
   constructor(){
+    //Interval de 500ms pour afficher dynamiquement la date et l'heure
     setInterval(() => {
       this.date = new Date()
     }, 500)
